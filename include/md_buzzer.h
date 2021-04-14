a71464f9e50d25e592b060e9bc49d078bd0e5281
MDFreak2 <75547040+MDFreak2@users.noreply.github.com>
2021-04-14 10:19:36 +0200

    new function DingDong

--- include/md_buzzer.h
+++ include/md_buzzer.h
@@ -2,9 +2,9 @@
   #define _MD_BUZZER_H_
 
   #include <Arduino.h>
-  #include "config.h"
+  #include "md_defines.h"
 
-  #ifdef USE_BUZZER
+  //#ifdef USE_BUZZER
     //
     // --- public prototypes
     //
@@ -13,14 +13,22 @@
         {
           public:  // methods
             md_buzzer(){};
-            void initMusic();
+            void initMusic(uint8_t buzzPin, uint8_t pwmChan);
             bool setSong(int16_t anzNotes, void* pnote0);
             bool setSong(int16_t anzNotes, void* pnote0, uint64_t beat_us);
+            void playDingDong(uint8_t anz = 1);
             #ifndef songTask
               void playSong();
             #endif
           protected: // deklarations
+            void playTone();
+
+            uint8_t  _pwmChan;
+            uint8_t  _buzzPin;
+            note_t   _note = (note_t) PAUSE;     // NOTE_C .. NOTE_B
+            int8_t   _octa = 0;     // oktave 0 .. 7
+            uint64_t _beat = 0;     // MB1 = base
         };
     //
-  #endif // USE_BUZZER
+//  #endif // USE_BUZZER
 #endif // _MD_BUZZER_H_
--- src/md_buzzer.cpp
+++ src/md_buzzer.cpp
@@ -1,168 +1,188 @@
 
 #include <Arduino.h>
-#include "config.h"
+#include "md_defines.h"
 
-#ifdef USE_BUZZER
-  #include "md_buzzer.h"
+#include "md_buzzer.h"
 
-  #ifdef USE_SONGTASK
-    TaskHandle_t songTask;
-  #endif
+#ifdef USE_SONGTASK
+  TaskHandle_t songTask;
+#endif
 
-  #define PLAYER_IS_FREE false
-  #define OCUPIED        true
+#define PLAYER_IS_FREE false
+#define OCUPIED        true
 
-  bool      _isfree;    // no song loaded
-  tone_t   *_psong;     // first note of song
-  uint16_t  _len;       // note count
-  double    _beatfac;       // OFF base beat = 1/4 length
+bool      _isfree;    // no song loaded
+tone_t   *_psong;     // first note of song
+uint16_t  _len;       // note count
+double    _beatfac;       // OFF base beat = 1/4 length
 
 
-  #ifdef USE_SONGTASK
-    void playSong(void * pvParameters)
-      {
-        note_t   _note = (note_t) PAUSE;     // NOTE_C .. NOTE_B
-        int8_t   _octa = 0;     // oktave 0 .. 7
-        uint64_t _beat = 0;     // MB1 = base
-        while(true)  // endless loop
-          {
-            if (!_isfree)
-              {
-                    #if (DEBUG_MODE >= CFG_DEBUG_ACTIONS)
-                      Serial.print(millis());
-                      Serial.println(" playSong .. ");
-                      Serial.println("for ii ");
-                    #endif
-                for (uint16_t ii = 0; ii < _len; ii++)
-                  {
-                     _note = (note_t) _psong[ii].note;
-                     _beat = _psong[ii].beat;
-                     _octa = _psong[ii].octa;
-
-                    if (_note == PAUSE)
-                      {
-                              #if (DEBUG_MODE >= CFG_DEBUG_DETAILS)
-                                Serial.print(millis());
-                                Serial.print(" ->PAUSE "); Serial.println(ii);
-                              #endif
-                        ledcWriteTone(PWM_BUZZ, 0.0);
-                      }
-                    else
-                      {
-                              #if (DEBUG_MODE >= CFG_DEBUG_DETAILS)
-                                Serial.print(millis());
-                                Serial.print(" ->NOTE "); Serial.print(_note);
-                                Serial.print(" ->OCTA "); Serial.println(_octa);
-                              #endif
-                        ledcWriteNote(PWM_BUZZ, _note, _octa);
-                      }
-                    usleep( (uint64_t) ((_beat * MUSIC_RATIO_P100 /100) * _beatfac) );          // play tone
-                    ledcWriteTone(PWM_BUZZ, 0.0);  // switch off
-                    usleep( (uint64_t) ((_beat * (100 - MUSIC_RATIO_P100) / 100) * _beatfac) ); // pause
-                  }
-                _isfree = true;
-              }
-            sleep(1);
-          }
-      }
-
-    void startSongTask()
-      {
-        Serial.println("startSongTask");
-        xTaskCreatePinnedToCore(
-                            playSong,   /* Task function. */
-                            "Task1",     /* name of task. */
-                            10000,       /* Stack size of task */
-                            NULL,        /* parameter of the task */
-                            4 | portPRIVILEGE_BIT, /* priority of the task */
-                            &songTask,      /* Task handle to keep track of created task */
-                            1              );          /* pin task to core 0 */
-      }
-
-  #else
-    void md_buzzer::playSong()
-      {
-        note_t   _note = (note_t) PAUSE;     // NOTE_C .. NOTE_B
-        int8_t   _octa = 0;     // oktave 0 .. 7
-        uint64_t _beat = 0;     // MB1 = base
-        if (!_isfree)
-          {
-                #if (DEBUG_MODE >= CFG_DEBUG_ACTIONS)
-                  Serial.print(millis());
-                  Serial.println(" playSong .. ");
-                  Serial.println("for ii ");
-                #endif
-            for (uint16_t ii = 0; ii < _len; ii++)
-              {
-                 _note = (note_t) _psong[ii].note;
-                 _beat = _psong[ii].beat;
-                 _octa = _psong[ii].octa;
-
-                if (_note == PAUSE)
-                  {
-                          #if (DEBUG_MODE >= CFG_DEBUG_DETAILS)
-                            Serial.print(millis());
-                            Serial.print(" ->PAUSE "); Serial.println(ii);
-                          #endif
-                    ledcWriteTone(PWM_BUZZ, 0.0);
-                  }
-                else
-                  {
-                          #if (DEBUG_MODE >= CFG_DEBUG_DETAILS)
-                            Serial.print(millis());
-                            Serial.print(" ->NOTE "); Serial.print(_note);
-                            Serial.print(" ->OCTA "); Serial.println(_octa);
-                          #endif
-                    ledcWriteNote(PWM_BUZZ, _note, _octa);
-                  }
-                usleep( (uint64_t) ((_beat * MUSIC_RATIO_P100 /100) * _beatfac) );          // play tone
-                ledcWriteTone(PWM_BUZZ, 0.0);  // switch off
-                usleep( (uint64_t) ((_beat * (100 - MUSIC_RATIO_P100) / 100) * _beatfac) ); // pause
-              }
-            _isfree = true;
-          }
-      }
-  #endif // USE_SONGTASK
-  void md_buzzer::initMusic()
+#ifdef USE_SONGTASK
+  void playSong(void * pvParameters)
     {
-      ledcAttachPin(PIN_BUZZ, PWM_BUZZ);
-      Serial.print(millis());
-      Serial.println(" initMusic .. ");
-      #ifdef USE_SONGTASK
-        startSongTask();
-      #endif
-      Serial.print(millis());
-      Serial.println(" .. initMusic finished");
-      _isfree = true;
-      _psong  = NULL;
-      usleep(500000);
+      note_t   _note = (note_t) PAUSE;     // NOTE_C .. NOTE_B
+      int8_t   _octa = 0;     // oktave 0 .. 7
+      uint64_t _beat = 0;     // MB1 = base
+      while(true)  // endless loop
+        {
+          if (!_isfree)
+            {
+                  #if (DEBUG_MODE >= CFG_DEBUG_ACTIONS)
+                    Serial.print(millis());
+                    Serial.println(" playSong .. ");
+                    Serial.println("for ii ");
+                  #endif
+              for (uint16_t ii = 0; ii < _len; ii++)
+                {
+                   _note = (note_t) _psong[ii].note;
+                   _beat = _psong[ii].beat;
+                   _octa = _psong[ii].octa;
+
+                  if (_note == PAUSE)
+                    {
+                            #if (DEBUG_MODE >= CFG_DEBUG_DETAILS)
+                              Serial.print(millis());
+                              Serial.print(" ->PAUSE "); Serial.println(ii);
+                            #endif
+                      ledcWriteTone(PWM_BUZZ, 0.0);
+                    }
+                  else
+                    {
+                            #if (DEBUG_MODE >= CFG_DEBUG_DETAILS)
+                              Serial.print(millis());
+                              Serial.print(" ->NOTE "); Serial.print(_note);
+                              Serial.print(" ->OCTA "); Serial.println(_octa);
+                            #endif
+                      ledcWriteNote(PWM_BUZZ, _note, _octa);
+                    }
+                  usleep( (uint64_t) ((_beat * MUSIC_RATIO_P100 /100) * _beatfac) );          // play tone
+                  ledcWriteTone(PWM_BUZZ, 0.0);  // switch off
+                  usleep( (uint64_t) ((_beat * (100 - MUSIC_RATIO_P100) / 100) * _beatfac) ); // pause
+                }
+              _isfree = true;
+            }
+          sleep(1);
+        }
     }
 
-  bool md_buzzer::setSong(int16_t anzNotes, void* pnote0)
+  void startSongTask()
     {
-      return setSong(anzNotes, pnote0, MUSIC_BASEBEAT_US);
+      Serial.println("startSongTask");
+      xTaskCreatePinnedToCore(
+                          playSong,   /* Task function. */
+                          "Task1",     /* name of task. */
+                          10000,       /* Stack size of task */
+                          NULL,        /* parameter of the task */
+                          4 | portPRIVILEGE_BIT, /* priority of the task */
+                          &songTask,      /* Task handle to keep track of created task */
+                          1              );          /* pin task to core 0 */
     }
 
-  bool md_buzzer::setSong(int16_t anzNotes, void* pnote0, uint64_t beat_us = MUSIC_BASEBEAT_US)
+#else
+  void md_buzzer::playSong()
     {
-      //tone_t  song[anzNotes];
-              Serial.print(millis());
-              Serial.print(" setSong anz = "); Serial.print(anzNotes);
-      if (_isfree)
-      {
-        _psong   = (tone_t*) pnote0;
-        _len     = anzNotes;
-        _isfree  = false;
-        _beatfac = beat_us / MUSIC_BASEBEAT_US;
-              Serial.println(" ok");
-        return ISOK;
-      }
-      else
-      {
-              Serial.println(" ERR Occupied");
-      }
-      return ISERR;
+      if (!_isfree)
+        {
+              #if (DEBUG_MODE >= CFG_DEBUG_ACTIONS)
+                Serial.print(millis());
+                Serial.println(" playSong .. ");
+                Serial.println("for ii ");
+              #endif
+          for (uint16_t ii = 0; ii < _len; ii++)
+            {
+               _note = (note_t) _psong[ii].note;
+               _beat = _psong[ii].beat;
+               _octa = _psong[ii].octa;
+               playTone();
+            }
+          _isfree = true;
+        }
     }
+#endif // USE_SONGTASK
 
-//
-#endif // USE_BUZZER
+void md_buzzer::playDingDong(uint8_t _anz)
+  {
+        #if (DEBUG_MODE >= CFG_DEBUG_ACTIONS)
+          Serial.print(millis());
+          Serial.println(" playingDong .. ");
+          Serial.println("for ii ");
+        #endif
+    _beat = MB4;
+    _octa = OP0;
+    _beatfac = 1;
+    for (uint16_t ii = 0; ii < _anz; ii++)
+      {
+        _note = (note_t) NOTE_G;
+        playTone();
+        _note = (note_t) NOTE_E;
+        playTone();
+      }
+  }
+
+void md_buzzer::playTone()
+  {
+    if (_note == PAUSE)
+      {
+              #if (DEBUG_MODE >= CFG_DEBUG_DETAILS)
+                Serial.print(millis());
+                Serial.print(" ->PAUSE "); Serial.println(ii);
+              #endif
+        ledcWriteTone(_pwmChan, 0.0);
+      }
+    else
+      {
+              #if (DEBUG_MODE >= CFG_DEBUG_DETAILS)
+                Serial.print(millis());
+                Serial.print(" ->NOTE "); Serial.print(_note);
+                Serial.print(" ->OCTA "); Serial.println(_octa);
+              #endif
+        ledcWriteNote(_pwmChan, _note, _octa);
+      }
+    usleep( (uint64_t) ((_beat * MUSIC_RATIO_P100 / 100) * _beatfac) );          // play tone
+    ledcWriteTone(_pwmChan, 0.0);  // switch off
+    usleep( (uint64_t) ((_beat * (100 - MUSIC_RATIO_P100) / 100) * _beatfac) ); // pause
+  }
+
+void md_buzzer::initMusic(uint8_t buzzPin, uint8_t pwmChan)
+  {
+    _buzzPin = buzzPin;
+    _pwmChan = pwmChan;
+    ledcAttachPin(buzzPin, pwmChan);
+    Serial.print(millis());
+    Serial.println(" initMusic .. ");
+    #ifdef USE_SONGTASK
+      startSongTask();
+    #endif
+    Serial.print(millis());
+    Serial.println(" .. initMusic finished");
+    _isfree = true;
+    _psong  = NULL;
+    usleep(500000);
+  }
+
+bool md_buzzer::setSong(int16_t anzNotes, void* pnote0)
+  {
+    return setSong(anzNotes, pnote0, MUSIC_BASEBEAT_US);
+  }
+
+bool md_buzzer::setSong(int16_t anzNotes, void* pnote0, uint64_t beat_us = MUSIC_BASEBEAT_US)
+  {
+    //tone_t  song[anzNotes];
+            Serial.print(millis());
+            Serial.print(" setSong anz = "); Serial.print(anzNotes);
+    if (_isfree)
+    {
+      _psong   = (tone_t*) pnote0;
+      _len     = anzNotes;
+      _isfree  = false;
+      _beatfac = beat_us / MUSIC_BASEBEAT_US;
+            Serial.println(" ok");
+      return ISOK;
+    }
+    else
+    {
+            Serial.println(" ERR Occupied");
+    }
+    return ISERR;
+  }
+
